22faf72a5ebb29415eebeb8c7c4969ca6613b1de
joselacruz <joselacruzperez@gmail.com>
2022-10-04 12:36:10 -0400

    show and delete img api

--- main.js
+++ main.js
@@ -2,76 +2,51 @@ const API_URL_RANDOM =
   "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_Tsb8bwOJBKDNjjdJc2XKB3i2c3rOOo2VU6CpHknnj23xp5vm4j8mzHCCINKY2A5f";
 
 const API_URL_FAVORITES =
-  "https://api.thecatapi.com/v1/favourites?limit=3&api_key=live_Tsb8bwOJBKDNjjdJc2XKB3i2c3rOOo2VU6CpHknnj23xp5vm4j8mzHCCINKY2A5f";
+  "https://api.thecatapi.com/v1/favourites?&api_key=live_Tsb8bwOJBKDNjjdJc2XKB3i2c3rOOo2VU6CpHknnj23xp5vm4j8mzHCCINKY2A5f";
+
+const API_URL_DELETE = (id) => ` https://api.thecatapi.com/v1/favourites/${id}?api_key=live_Tsb8bwOJBKDNjjdJc2XKB3i2c3rOOo2VU6CpHknnj23xp5vm4j8mzHCCINKY2A5f`;
 
 const botoncito = document.querySelector("button");
 const containerImg = document.querySelector(".random-cats");
 const favoritesContain = document.querySelector(".favorites-cats");
 const spanError = document.querySelector("#error");
-botoncito.addEventListener('click', () => viewCat(API_URL_RANDOM,containerImg,"Saved Cat and Favorite"));
+botoncito.addEventListener("click", () => viewRandomCat());
 
-const templateCards = (imgUrl, name) => {
+const templateCards = (imgUrl, name, idImg) => {
   return `<article>
     <figure>
-    <img src = ${imgUrl}>
+    <img src = ${imgUrl} id = "${idImg}">
     </figure>
     <button class = "btn2"> ${name} </button>
-    </article>`
+    </article>`;
 };
 
 async function viewCat(Apiurl, sectionName, nameButton) {
   const response = await fetch(Apiurl);
   const data = await response.json();
-  console.log("data", data);
 
   if (response.status !== 200) {
     sectionName.innerHTML = `Hubo un Error ${response.status} ${data.message}`;
-  } 
-  else {
+  } else {
     const cards = data.map((item) => {
-      return templateCards(item.url || item.image.url , nameButton);
+      return templateCards(item.url || item.image.url, nameButton, item.id);
     });
-  
-    sectionName.innerHTML = cards.join('');
-    console.log(cards, "Quien es cards" );
-
+    
+    sectionName.innerHTML = await cards.join("");
   }
 }
 
 async function viewRandomCat() {
   await viewCat(API_URL_RANDOM, containerImg, "Saved Cat and Favorite");
-  const obtain = containerImg.childNodes;
-  obtain.forEach( button => {
-    const bottonEvent = button.getElementsByTagName('button')[0];
-    bottonEvent.addEventListener("click", (event) => {
-     console.log("me Llame");
-     viewFavoriteCat();
-     let imgSelect =  event.path[1].childNodes[1].getElementsByTagName('img')[0];
-    
-    })
- 
- })
-};
+  getButtomOfTemplateHTML(containerImg, saveFavoritesCat);
+}
 viewRandomCat();
-async function viewFavoriteCat() {
-    await viewCat(API_URL_FAVORITES, favoritesContain, "Deleted Cat Favorite");
 
-    const obtain = favoritesContain.childNodes;
-    obtain.forEach( button => {
-       const bottonEvent = button.getElementsByTagName('button')[0];
-       bottonEvent.addEventListener("click", (event) => {
-        console.log("me Llame log");
-        let imgSelect =  event.path[1].childNodes[1].getElementsByTagName('img')[0];
-       
-       })
-    
-    })
-    
+async function viewFavoriteCat() {
+  await viewCat(API_URL_FAVORITES, favoritesContain, "Deleted Cat Favorite");
+   getButtomOfTemplateHTML(favoritesContain,deleteFavoritesCat);
 }
 viewFavoriteCat();
-
-
-
 
 async function saveFavoritesCat(id) {
   const response = await fetch(API_URL_FAVORITES, {
@@ -83,7 +58,27 @@ async function saveFavoritesCat(id) {
       image_id: id,
     }),
   });
-  console.log("Response", response);
+  viewFavoriteCat();
+}
+ async function deleteFavoritesCat(id) {
+   const response = await fetch(API_URL_DELETE(id), {
+    method: "DELETE",
+ });
+viewFavoriteCat();
+  
+
+ }
+
+function getButtomOfTemplateHTML(botonTagName, starFunction) {
+  const obtain = botonTagName.childNodes;
+  obtain.forEach((button) => {
+    const bottonEvent = button.getElementsByTagName("button")[0];
+    bottonEvent.addEventListener("click", (event) => {
+      const idImgSelect =
+        event.path[1].childNodes[1].getElementsByTagName("img")[0].id;
+        console.log("ID OR" , idImgSelect);
+      starFunction(idImgSelect);
+    });
+  });
 }
 
-saveFavoritesCat();
